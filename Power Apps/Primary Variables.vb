47517271e55e18c397df55841b0066820e3cecc0
Steven-D-Morgan <47731992+Steven-D-Morgan@users.noreply.github.com>
2023-12-07 14:03:43 -0500

    g

---
+++ Power Apps/Primary Variables.vb
@@ -0,0 +1,12 @@
+Set(varMicrosoftUserDisplayName, Office365Users.MyProfile().DisplayName);
+Set(varCurrentUser, Office365Users.MyProfile());
+
+Set(varCushmanRed, RGBA(228,0,43,0.5));
+Set(varCushmanDarkRed, RGBA(166,25,46,0.5));
+Set(varCushmanOrange, RGBA(255,103,31,0.5));
+Set(varCushmanDarkGrey, RGBA(105,107,107,0.5));
+Set(varCushmanGrey, RGBA(150,150,150,0.5));
+Set(varCushmanDarkBlue, RGBA(0,56,101,0.5));
+Set(varCushmanBlue, RGBA(0,147,178,0.5));
+Set(varCushmanLightBlue, RGBA(155,211,221,0.5));
+Set(varCushmanLime, RGBA(181,189,0,0.5));
