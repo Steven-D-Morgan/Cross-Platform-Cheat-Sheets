eddf4f73268dffe8a436405946d4214788ea3808
Steven-D-Morgan <47731992+Steven-D-Morgan@users.noreply.github.com>
2023-12-12 14:10:56 -0500

    h

--- Power Apps/Primary Variables.vb
+++ Power Apps/Primary Variables.vb
@@ -1,4 +1,5 @@
 Set(varMicrosoftUserDisplayName, Office365Users.MyProfile().DisplayName);
+Set(varUsergivenname,Office365Users.MyProfile().GivenName);
 Set(varCurrentUser, Office365Users.MyProfile());
 
 Set(varCushmanRed, RGBA(228,0,43,0.5));
@@ -10,3 +11,10 @@ Set(varCushmanDarkBlue, RGBA(0,56,101,0.5));
 Set(varCushmanBlue, RGBA(0,147,178,0.5));
 Set(varCushmanLightBlue, RGBA(155,211,221,0.5));
 Set(varCushmanLime, RGBA(181,189,0,0.5));
+
+Set(varNightBackgroundColor, ColorValue("#3B2F2F"));
+Set(varNightTextColor, ColorValue("#f3f6f4"));
+Set(varDayBackgroundColor, ColorValue("#E4D00A"));
+Set(varDayTextColor, ColorValue("#000000"));
+Set(varAfternoonBackgroundColor, ColorValue("#CD7F32"));
+Set(varAfternoonTextColor, ColorValue("#f3f6f4"));
